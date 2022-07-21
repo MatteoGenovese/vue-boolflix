3f3f0abc979c319c6cf4826dd54c81126a46e783
MatteoGenovese <matteo.genovese@icloud.com>
2022-07-21 12:58:06 +0200

    created Header and Main components

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+<header>
+  ciao sono l'header
+</header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    ciao sono il main
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+
+
+}
+</script>
+
+<style>
+
+</style>
