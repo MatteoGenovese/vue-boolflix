e1c9d399b9142fbe036ab4b7a4831952f8bbd9a7
MatteoGenovese <matteo.genovese@icloud.com>
2022-07-22 16:15:43 +0200

    feat: added the card swap to tv series

--- src/components/Serie.vue
+++ src/components/Serie.vue
@@ -1,28 +1,31 @@
 <template>
-    <div class="tv-serie">
+    <div>
+        <transition name="rotate">
+            <div class="tv-serie" @click="showFront=(!showFront)">
+                <div class="front" v-if="showFront">
+                    <img v-if="tvSerie.poster_path != null" :src="basicImageUrl + poster_sizes[3] + tvSerie.poster_path"
+                        :alt="tvSerie.poster_path">
+                    <img v-else :src="basicImageUrl + poster_sizes[3] + tvSerie.poster_path"
+                        class="d-flex justify-content-center" :alt="tvSerie.name">
+                </div>
+                <div class="back" v-else>
+                    <ul>
+                        <li>Titolo: {{ tvSerie.name }}</li>
+                        <li>Titolo Originale: {{ tvSerie.original_name }}</li>
+                        <li>Lingua:
+                            <country-flag :country='tvSerie.original_language' size='normal' />
+                        </li>
+                        <li>
+                            <star-rating :increment="1" :max-rating="5" inactive-color="#999999" active-color="#F8D36B"
+                                :star-size="20" :read-only="true" :rating="tvSerie.vote_average / 2">
+                            </star-rating>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </transition>
 
-        <div class="front">
-            <img v-if=" tvSerie.poster_path != null" :src="basicImageUrl + poster_sizes[3] + tvSerie.poster_path" :alt="tvSerie.poster_path">
-            <img v-else :src="basicImageUrl + poster_sizes[3] + tvSerie.poster_path" class="d-flex justify-content-center" :alt="tvSerie.name">
-        </div>
-        <div class="back d-none">
-            <ul>
-                <li>Titolo: {{ tvSerie.name }}</li>
-                <li>Titolo Originale: {{ tvSerie.original_name }}</li>
-                <li>Lingua:
-                    <country-flag :country='tvSerie.original_language' size='normal' />
-                </li>
-                <li>
-                    <star-rating :increment="1" :max-rating="5" inactive-color="#999999" active-color="#F8D36B"
-                        :star-size="20" :read-only="true" :rating="tvSerie.vote_average / 2">
-                    </star-rating>
-                </li>
-            </ul>
-        </div>
     </div>
-
-
-
 </template>
 
 <script>
@@ -55,7 +58,9 @@ export default {
                 "w500",
                 "w780",
                 "original"
-            ]
+            ],
+            showFront:true,
+            showBack:false,
         }
     },
 
@@ -77,4 +82,17 @@ export default {
         object-fit: cover;
     }
 }
+
+.rotate-enter-active {
+    animation: finished 2.5s reverse;
+  }
+  .rotate-leave-active {
+    animation: finished 2.5s;
+  }
+  @keyframes finished {
+    0% { opacity: 1; top: 0;}
+    50% { opacity: 1; top: 0;}
+    60% { opacity: 1; }
+    100% { opacity: 0; top: -100vh;}
+  }
 </style>
