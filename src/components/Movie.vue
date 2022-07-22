127408f00250d0984129faa44a937cfa358ff1c5
MatteoGenovese <matteo.genovese@icloud.com>
2022-07-22 16:09:14 +0200

    feat: swap movies card

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,27 +1,31 @@
 <template>
-    <div class="movie">
-        <div class="front ">
+    <div>
+        <transition name="rotate">
+            <div class="movie" @click="showFront=(!showFront)">
+                <div class="front" v-if="showFront">
+                    <img v-if="movie.poster_path != null" :src="basicImageUrl + poster_sizes[3] + movie.poster_path"
+                        :alt="movie.poster_path">
+                    <img v-else :src="basicImageUrl + poster_sizes[3] + movie.poster_path"
+                        class="d-flex justify-content-center" :alt="movie.title">
+                </div>
+                <div class="back" v-else>
+                    <ul>
+                        <li>Titolo: {{ movie.title }}</li>
+                        <li>Titolo Originale: {{ movie.original_title }}</li>
+                        <li>Lingua:
+                            <country-flag :country='movie.original_language' size='normal' />
+                        </li>
+                        <li>
+                            <star-rating :increment="1" :max-rating="5" inactive-color="#999999" active-color="#F8D36B"
+                                :star-size="20" :read-only="true" :rating="movie.vote_average / 2">
+                            </star-rating>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </transition>
 
-            <img v-if=" movie.poster_path != null" :src="basicImageUrl + poster_sizes[3] + movie.poster_path" :alt="movie.poster_path">
-            <img v-else :src="basicImageUrl + poster_sizes[3] + movie.poster_path" class="d-flex justify-content-center" :alt="movie.title">
-
-        </div>
-        <div class="back d-none">
-            <ul>
-                <li>Titolo: {{ movie.title }}</li>
-                <li>Titolo Originale: {{ movie.original_title }}</li>
-                <li>Lingua:
-                    <country-flag :country='movie.original_language' size='normal' />
-                </li>
-                <li>
-                    <star-rating :increment="1" :max-rating="5" inactive-color="#999999" active-color="#F8D36B"
-                        :star-size="20" :read-only="true" :rating="movie.vote_average / 2">
-                    </star-rating>
-                </li>
-            </ul>
-        </div>
     </div>
-
 </template>
 
 <script>
@@ -54,7 +58,10 @@ export default {
                 "w500",
                 "w780",
                 "original"
-            ]
+            ],
+            showFront:true,
+            showBack:false,
+
         }
     },
 
@@ -62,18 +69,34 @@ export default {
 </script>
 
 <style lang="scss">
-.movie, .tv-serie{
+.movie,
+.tv-serie {
     border: 1px solid black;
-    width: calc( ( 100vw / 5 ) - 10px );
+    width: calc((100vw / 5) - 10px);
     display: flex;
     flex-direction: column;
     justify-content: center;
-    ul{
+
+    ul {
         list-style: none;
     }
-    img{
+
+    img {
         height: 100%;
         object-fit: cover;
     }
 }
+
+.rotate-enter-active {
+    animation: finished 2.5s reverse;
+  }
+  .rotate-leave-active {
+    animation: finished 2.5s;
+  }
+  @keyframes finished {
+    0% { opacity: 1; top: 0;}
+    50% { opacity: 1; top: 0;}
+    60% { opacity: 1; }
+    100% { opacity: 0; top: -100vh;}
+  }
 </style>
